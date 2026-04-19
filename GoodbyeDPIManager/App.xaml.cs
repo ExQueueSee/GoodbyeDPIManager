1af834006f02bca97109657b66e88ad5c9854205
ExQueueSee <ari2003ata@gmail.com>
2026-04-19 10:38:23 +0300

    feat: add AppUserModelID for Windows shell integration
    Added P/Invoke for `SetCurrentProcessExplicitAppUserModelID` and overrode `OnStartup` to set a unique App ID. This ensures proper taskbar grouping, Start Menu pinning options, and notification handling.

--- GoodbyeDPIManager/App.xaml.cs
+++ GoodbyeDPIManager/App.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace GoodbyeDPIManager
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern void SetCurrentProcessExplicitAppUserModelID([MarshalAs(UnmanagedType.LPWStr)] string AppID);
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // This string acts as your app's unique identity in Windows
+            SetCurrentProcessExplicitAppUserModelID("AtaberkTekin.GoodbyeDPIManager");
+            base.OnStartup(e);
+        }
+    }
 }
